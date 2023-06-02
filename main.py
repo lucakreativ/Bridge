53fc480af89f0977bce7145e45d34e0741f571d2
ich <net.a@gmx.de>
2023-06-02 20:34:42 +0200

    added class deck and class player, shuffle and deal functions

--- main.py
+++ main.py
@@ -1,32 +1,81 @@
-class Color():
-    def __init__(self, int_rep=None, char_rep=None, str_rep=None):
+import random as rd
+
+
+class Color:
+    def __init__(self, col):
         chars = ['c', 'd', 'h', 's']
         strs = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-        if int_rep!=None:
-            self.int_rep = int_rep
-            self.char_rep = chars[int_rep]
-            self.str_rep = strs[int_rep]
-        elif char_rep!=None:
-            self.int_rep = chars.index(char_rep)
-            self.char_rep = char_rep
+        if type(col) == int:
+            self.int_rep = col
+            self.char_rep = chars[col]
+            self.str_rep = strs[col]
+        elif type(col) == str and len(col) == 1:
+            self.int_rep = chars.index(col)
+            self.char_rep = col
             self.str_rep = strs[self.int_rep]
-        elif str_rep!=None:
-            self.int_rep = strs.index(str_rep)
+        elif type(col) == str:
+            self.int_rep = strs.index(col)
             self.char_rep = chars[self.int_rep]
-            self.str_rep = str_rep
+            self.str_rep = col
         else:
             raise TypeError('Color() missing an argument')
+
     def __str__(self):
-        return(self.str_rep)
+        return self.str_rep
 
 
-class Card():
+class Value:
+    def __init__(self, val):
+        chars = ['2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A']
+        strs = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        if type(val) == int:
+            self.int_rep = val
+            self.char_rep = chars[val]
+            self.str_rep = strs[val]
+        elif type(val) == str and len(val) == 1:
+            self.int_rep = chars.index(val)
+            self.char_rep = val
+            self.str_rep = strs[self.int_rep]
+        elif type(val) == str:
+            self.int_rep = strs.index(val)
+            self.char_rep = chars[self.int_rep]
+            self.str_rep = val
+        else:
+            raise TypeError('Value missing an argument')
+
+    def __str__(self):
+        return self.str_rep
+
+
+class Card:
+
     def __init__(self, color, value):
-        """
+        self.col = Color(color)
+        self.val = Value(value)
 
-        :param color: str, int or char
-        :param value: int
-            values from 2 to 14, 2-10 = 2-10, 11 = Jack, 12 = Queen, 13 = King, 14 = Ace
-        """
-        self.color = Color(char_rep=color)
-        self.value = value
+    def __str__(self):
+        return self.col.__str__() + ' ' + self.val.__str__()
+
+
+class Deck:
+    def __init__(self, shuffled=True):
+        self.cards = []
+        for col in range(4):
+            for val in range(12):
+                self.cards.append(Card(col, val))
+        if shuffled:
+            self.shuffle()
+
+    def shuffle(self):
+        rd.shuffle(self.cards)
+
+    def deal(self, n, o, s, w):
+        n.cards = self.cards[0:13]
+        o.cards = self.cards[13:26]
+        s.cards = self.cards[26:39]
+        w.cards = self.cards[39:52]
+
+class Player:
+    def __init__(self, name, position = 'N'):
+        self.name = name
+        self.position = position
