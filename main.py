b888ea34546debb13c1aeb65498aa3b5a87d4057
ich <net.a@gmx.de>
2023-06-03 22:29:07 +0200

    cleaned up the code added some code for bidding, not functional yet

--- main.py
+++ main.py
@@ -1,67 +1,45 @@
 import random as rd
 
-
-class Color:
-    def __init__(self, col):
-        chars = ['c', 'd', 'h', 's']
-        strs = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-        if type(col) == int:
-            self.int_rep = col
-            self.char_rep = chars[col]
-            self.str_rep = strs[col]
-        elif type(col) == str and len(col) == 1:
-            self.int_rep = chars.index(col)
-            self.char_rep = col
-            self.str_rep = strs[self.int_rep]
-        elif type(col) == str:
-            self.int_rep = strs.index(col)
-            self.char_rep = chars[self.int_rep]
-            self.str_rep = col
-        else:
-            raise TypeError('Color() missing an argument')
-
-    def __str__(self):
-        return self.char_rep
-
-
-class Value:
-    def __init__(self, val):
-        chars = ['2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A']
-        strs = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-        if type(val) == int:
-            self.int_rep = val
-            self.char_rep = chars[val]
-            self.str_rep = strs[val]
-        elif type(val) == str and len(val) == 1:
-            self.int_rep = chars.index(val)
-            self.char_rep = val
-            self.str_rep = strs[self.int_rep]
-        elif type(val) == str:
-            self.int_rep = strs.index(val)
-            self.char_rep = chars[self.int_rep]
-            self.str_rep = val
-        else:
-            raise TypeError('Value missing an argument')
-
-    def __str__(self):
-        return self.char_rep
-
-
 class Card:
 
-    def __init__(self, color, value):
-        self.col = Color(color)
-        self.val = Value(value)
+    def __init__(self, col, val):
+        col_chars = ['c', 'd', 'h', 's']
+        col_strs = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+        val_chars = ['2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A']
+        val_strs = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        if type(col) == int:
+            self.col = col
+            self.col_char = col_chars[col]
+            self.col_str = col_strs[col]
+        elif type(col) == str and len(col) == 1:
+            self.col_char = col_chars.index(col)
+            self.col = col
+            self.col_str = col_strs[self.col]
+        elif type(col) == str:
+            self.col = col_strs.index(col)
+            self.char_rep = col_chars[self.col]
+            self.str_rep = col
+        if type(val) == int:
+            self.val = val
+            self.val_char = val_chars[val]
+            self.val_str = val_strs[val]
+        elif type(val) == str and len(val) == 1:
+            self.val = val_chars.index(val)
+            self.val_char = val
+            self.val_str = val_strs[self.val]
+        elif type(val) == str:
+            self.val = val_strs.index(val)
+            self.val_char = val_chars[self.val]
+            self.val_str = val
 
-    def __str__(self):
-        return self.col.__str__() + '' + self.val.__str__()
-
+    def __repr__(self):
+        return self.col_char + '' + self.val_char
 
 class Deck:
     def __init__(self, shuffled=True):
         self.cards = []
         for col in range(4):
-            for val in range(12):
+            for val in range(13):
                 self.cards.append(Card(col, val))
         if shuffled:
             self.shuffle()
@@ -76,21 +54,52 @@ class Deck:
         player[2].cards = self.cards[26:39]
         player[3].cards = self.cards[39:52]
 
-
 class Player:
     def __init__(self, name=None, pos='n'):
         self.name = name
         self.pos = pos
+        self.bids = []
+        self.cards = []
 
     def play(self):
-        return self.cards.pop(rd.randint(0, len(self.cards)-1))
+        return self.cards.pop(rd.randint(0, len(self.cards) - 1))
 
+    def bid(self, bids):
+        forum_d(self.cards, bids)
 
-def game(player):
+class Bids:
+    def __init__(self):
+        cols = ['c', 'd', 'h', 's', 'n']
+        self.bids = ["p"]
+        for val in range(7):
+            for col in range(5):
+                self.bids.append({
+                    "name": f"{val} {cols[col]}",
+                    "bidder": None,
+                    "val": val,
+                    "col": col
+                })
+        highest_bid = "p"
+
+def game(player=None):
+    if player == None: player = [Player(pos=i) for i in range(4)]
     deck = Deck()
     deck.deal(player)
-    current_player = 0
+    bidding(player)
+    playing(player)
+
+def playing(player):
     for i in range(13):
         for j in range(i, i + 4):
-            print(player[i % 4].play())
+            print(player[j % 4].play(), end=' - ')
+        print('')
 
+def bidding(player):
+    bids = Bids
+    i = 0
+    while True:
+        player[i].bid()
+        i+=1
+        i=i%4
+
+game()
